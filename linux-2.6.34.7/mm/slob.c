69e60b633aa45a00694c44a54731d1ec217238d3
Torben Rasmussen <rtorben@gmail.com>
2011-04-25 17:24:28 -0700

    added some printks to see what is going on with sp and b

--- linux-2.6.34.7/mm/slob.c
+++ linux-2.6.34.7/mm/slob.c
@@ -70,6 +70,10 @@
 #include <linux/kmemleak.h>
 #include <asm/atomic.h>
 
+/* CS411 stuff */
+unsigned int sys_get_slob_amt_claimed(void);
+unsigned int sys_get_slob_amt_free(void);
+
 /*
  * slob_block has a field 'units', which indicates size of block if +ve,
  * or offset of next block if -ve (in SLOB_UNITs).
@@ -364,19 +368,31 @@ static void *slob_alloc(size_t size, gfp_t gfp, int align, int node)
 
 	/* Not enough space: must allocate a new page */
 	if (!b) {
+        /* CS411
+         * We need to add something here that keeps track of
+         * small memory allocations */
+
+        printk("sp: %d\n", sp);
+
 		b = slob_new_pages(gfp & ~__GFP_ZERO, 0, node);
+		printk("b = slob_new_pages(gfp & ~__GFP_ZERO, 0, node);\n\tb = %d\n", b);
 		if (!b)
 			return NULL;
 		sp = slob_page(b);
+		printk("sp = slob_page(b);\n\tsp->units = %d\n", sp->units);
+		printk("sp = slob_page(b);\n\tsp->free = %d\n", sp->free);
 		set_slob_page(sp);
 
 		spin_lock_irqsave(&slob_lock, flags);
 		sp->units = SLOB_UNITS(PAGE_SIZE);
 		sp->free = b;
+		printk("sp->units = SLOB_UNITS(PAGE_SIZE);\n\tsp->units = %d\n", sp->units);
+		printk("sp->free = b;\n\tsp->free = %d\n", sp->free);
 		INIT_LIST_HEAD(&sp->list);
 		set_slob(b, SLOB_UNITS(PAGE_SIZE), b + SLOB_UNITS(PAGE_SIZE));
 		set_slob_page_free(sp, slob_list);
 		b = slob_page_alloc(sp, size, align);
+		printk("b = slob_page_alloc(sp, size, align);\n\tb = %d\n", b);
 		BUG_ON(!b);
 		spin_unlock_irqrestore(&slob_lock, flags);
 	}
@@ -696,4 +712,14 @@ void __init kmem_cache_init(void)
 void __init kmem_cache_init_late(void)
 {
 	/* Nothing to do */
+}
+
+unsigned int sys_get_slob_amt_claimed(void)
+{
+    return 0;
+}
+
+unsigned int sys_get_slob_amt_free(void)
+{
+    return 0;
 }
