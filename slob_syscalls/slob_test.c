f9dfe791ab757ad13387c144e8c0929dd7fd2d06
Torben Rasmussen <rtorben@gmail.com>
2011-04-28 16:55:46 -0700

    added other syscalls

--- slob_syscalls/slob_test.c
+++ slob_syscalls/slob_test.c
@@ -1,15 +1,28 @@
 #include <linux/unistd.h> 
 #include <sys/syscall.h> 
 #include <stdio.h>
+#include <unistd.h>
 
 #define __NR_get_slob_amt_claimed 338
 #define __NR_get_slob_amt_free 339
 
 int main() 
 { 
-    printf("running syscalls\n");
+    printf("Checking current memory usage...\n");
     syscall( __NR_get_slob_amt_claimed );
     syscall( __NR_get_slob_amt_free );
-    printf("done running syscalls\n");
+    printf("Done checking current memory usage!\n");
+
+    printf("Running command...\n");
+    system("cat /proc/cpuinfo| grep [0-9] | sort | tr cpu ppu");
+    printf("Done running command!\n");
+
+    printf("Checking new memory usage...\n");
+    syscall( __NR_get_slob_amt_claimed );
+    syscall( __NR_get_slob_amt_free );
+    printf("Done checking new memory usage!\n");
+
+    system("dmesg | tail --lines=4");
+
     return 0; 
 }
