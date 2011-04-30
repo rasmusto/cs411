fd9c495c1b3b75815df51d019f5f065e9e9e5926
Torben Rasmussen <rtorben@gmail.com>
2011-04-29 23:20:31 -0700

    fix dis

--- create_tarball.sh
+++ create_tarball.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #Create diff of kernel with clean copy
-git diff 08ccd linux-2.6.34.7 > ./2.6.34.7-my_syscall-team08.patch
+git diff 08ccd linux-2.6.34.7 > ./2.6.34.7-proj02.patch
 
 #Create git log file
 git log > git.log
 
-tar cvf proj01.tar.gz /boot/vmlinuz-my_syscall-team08
-tar rvf proj01.tar.gz proj01-iosched
-tar rvf proj01.tar.gz proj01-syscall
-tar rvf proj01.tar.gz 2.6.34.7-my_syscall-team08.patch
-tar rvf proj01.tar.gz git.log
+tar cvf proj02.tar.gz /boot/vmlinuz-2.6.34-7-proj02
+tar rvf proj02.tar.gz slob_syscalls
+tar rvf proj02.tar.gz 2.6.34.7-proj02.patch
+tar rvf proj02.tar.gz git.log
 
 #Clean up files
-rm 2.6.34.7-my_syscall-team08.patch
+rm 2.6.34.7-proj02.patch
 rm git.log
